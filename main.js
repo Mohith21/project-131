b966aafa487d9714ebdda9e4a9b6e20fdc79ce63
Mohith21 <77101130+Mohith21@users.noreply.github.com>
2022-04-25 15:33:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function page1()
+{
+    window.location = "bedroom.html";
+}
+
+function page2()
+{
+    window.location = "tv.html";
+}
+
+function page3()
+{
+    window.location = "desk.html";
+}
+
+function page4()
+{
+    window.location = "bottles.html";
+}
+
+function page5()
+{
+    window.location = "fruit.html";
+}
---
+++ tv.js
@@ -0,0 +1,34 @@
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage("tv.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(500, 300);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(result);
+}
+
+function draw() {
+    image(img, 0, 0, 500, 300);
+
+}
+
+function back2() {
+    window.location = "index.html";
+}
